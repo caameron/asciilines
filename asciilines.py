a2648ed443515940e0dab2db55f15d8033e836f6
CaameronNakasone <caameron@pdx.edu>
2019-06-29 20:36:12 -0700

    added test file and reading in argument file

--- asciilines.py
+++ asciilines.py
@@ -5,4 +5,7 @@ import sys
 
 
 #Read in file from the command line if no file display error
-
+if len(sys.argv) == 1:
+    print("No file provided, please run program with file")
+else:
+    print("THANK YOU")
